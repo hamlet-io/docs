35d89c41f2f42a1a6e214f8c54ff0b5750b5bde2
Mitchell Hartigan <mitch.hartigan@gmail.com>
2020-04-17 16:39:30 +1000

    feat: formatted properties into table

--- src/pages/reference.js
+++ src/pages/reference.js
@@ -1,102 +1,120 @@
-import React, { useState, useEffect, Component } from 'react';
-import Layout from '@theme/Layout';
-import axios from 'axios';
+import React, { useState, useEffect, Component } from "react";
+import Layout from "@theme/Layout";
+import axios from "axios";
 
 /*
  alert(JSON.stringify(components[0], null, 4));
 */
 
-const schemaUrl = "https://hamlet-docs-git-add-schema-documents.hamlet.now.sh/schema/latest/blueprint/component.json";
+const schemaUrl =
+  "https://hamlet-docs-git-add-schema-documents.hamlet.now.sh/schema/latest/blueprint/component.json";
 const filterObjects = ["deployment-units", "instances"];
 const filterAttributerObjects = ["DeploymentUnits", "Instances"];
 
 const getAsyncDefinitions = () => {
   let components = [];
-  return axios.get(schemaUrl).then(response => {
-    Object.entries(response.data.definitions)
-      .map(definition => {
-        let [name, value] = definition; 
+  return axios.get(schemaUrl).then((response) => {
+    Object.entries(response.data.definitions).map((definition) => {
+      let [name, value] = definition;
 
-        /* Filter Components */
-        if (! filterObjects.includes(name)) {
+      /* Filter Components */
+      if (!filterObjects.includes(name)) {
+        /* Filter Attributes */
+        let attributes = [];
+        Object.entries(value.properties).map((attr) => {
+          let [key, val] = attr;
+          if (!filterAttributerObjects.includes(key)) {
+            attributes.push({ name: key, value: val });
+          }
+          return attributes;
+        });
 
-          /* Filter Attributes */
-          let attributes = [];
-          Object.entries(value.properties)
-            .map(attr => {
-              let [key, val] = attr;
-              if (! filterAttributerObjects.includes(key)){
-                attributes.push({ 'name': key, 'value': val })
-              }
-              return attributes
-            });
-
-          components.push(
-            { 
-              'name': name,
-              'attributes': attributes
-            }
-          )
-        }
-        return components
-      });
-    return { 'components': components }
+        components.push({
+          name: name,
+          attributes: attributes,
+        });
+      }
+      return components;
+    });
+    return { components: components };
   });
-}
+};
 
 function HamletAttribute(props) {
-  return <p>{props.name}</p>
+  const { attribute } = props;
+  return (
+    <tr>
+      <td>{attribute.name}</td>
+      <td>FALSE</td>
+      <td>{attribute.value.type}</td>
+      <td>{attribute.value.default || "n/a"}</td>
+      <td>{attribute.value.description || "n/a"}</td>
+    </tr>
+  );
 }
 
-
 function HamletComponent(props) {
-  const [componentState, setComponentState] = React.useState(
-    { 
-      attributes : props.attributes
-    });
+  const [componentState, setComponentState] = React.useState({
+    attributes: props.attributes,
+  });
 
   return (
     <div>
       <h1>{props.name}</h1>
-      <div className="AttributesContainer">
-        {
-          componentState.attributes.map((attribute, index) => {
-            return <HamletAttribute key={index} name={attribute.name} />
-          })
-        }
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Attributes</th>
+            <th>Mandatory</th>
+            <th>Type</th>
+            <th>Default</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {componentState.attributes.map((attribute) => {
+            return <HamletAttribute attribute={attribute} />;
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
 const HamletComponents = () => {
   const [definitions, setDefinitions] = React.useState({ components: [] });
 
   React.useEffect(() => {
-    getAsyncDefinitions().then(result => {
+    getAsyncDefinitions().then((result) => {
       setDefinitions(result);
     });
   }, []);
 
-
-  return(
+  return (
     <Layout id="HamletComponents">
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50vh',
-            fontSize: '20px',
-          }}>
-          {
-            definitions.components.map((component, index) => {
-              return <HamletComponent key={index} name={component.name} attributes={component.attributes} />
-            })
-          }
-        </div>
-      </Layout>
+      <div
+        style={{
+          marginLeft: "10vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          height: "100%",
+          fontSize: "20px",
+        }}
+      >
+        {definitions.components.map((component, index) => {
+          return (
+            <HamletComponent
+              key={index}
+              name={component.name}
+              attributes={component.attributes}
+            />
+          );
+        })}
+      </div>
+    </Layout>
   );
-}
+};
 
 export default HamletComponents;
