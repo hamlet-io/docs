3de7d411b7a41047c2ddae98fee4e54c79f31329
Ross Murray <rossmurray@protonmail.com>
2021-01-22 16:32:07 +1100

    refactor: references index page to include event handler for changes
    This commit includes a new component event handler "handleUpdate".
    It is intended to be passed to button "onClick" events that would
    allow a menu to be configured to load a different schema for each button.
    
    example:
    
    ```
    // a Button component definition that accepts an "onClick" event handler
    const Button = ({ name, value, onClick }) => <button type="button" value={value} onClick={onClick}>{name}</button>;
    
    // an instance of Button
    <Button name="component" value="component" onClick={handleUpdate} />
    ```

--- src/pages/reference/index.js
+++ src/pages/reference/index.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 import Layout from "@theme/Layout";
 import HamletDataTables from '@site/src/components/DataTables';
 
 const HamletComponentsSchema = () => {
+
+  const [dataType, setDataType] = useState('component');
+
+  const handleUpdate = event => setDataType(event.target.value);
+
   return (
-    <Layout id="HamletComponents">
+      <Layout id="HamletComponents">
         <div className="row">
           <div className="col col--1" />
           <div className="col col--10 component">
-            <HamletDataTables type="component" />
+            <HamletDataTables type={dataType} />;
           </div>
           <div className="col col--1" />
         </div>
