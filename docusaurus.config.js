ad56b01978b4ee1048d5e2ca9d3d35c3e0d65a2d
Michael Foley <roleyfoley@gmail.com>
2022-12-14 09:02:10 +1100

    feat: use typesense local cluster for search (#184)
    - Uses a gosource provided typesense search cluster to enable
    search across the docs site. The agolia service didn't provide
    what we needed ( or a response )

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+require('dotenv').config()
+
+// Allow for overrides of typesense config at build time
+const typesense_host = process.env.TYPESENSE_SEARCH_HOST || "docs.hamlet.io"
+const typesense_protocol = process.env.TYPESENSE_SEARCH_PROTOCOL || "https"
+const typesense_port = parseInt(process.env.TYPESENSE_SEARCH_PORT || "") || 443
+const typesense_search_api_key = process.env.TYPESENSE_SEARCH_API_KEY || "abcdef123"
+const typesense_index_name = process.env.TYPESENSE_INDEX_NAME || "docs-hamlet-io"
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'hamlet',
   tagline: 'Deploy anywhere, at scale',
   url: 'https://docs.hamlet.io',
@@ -9,6 +22,16 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: 'throw',
+
+  markdown: {
+    mermaid: true
+  },
+
+  themes: [
+    '@docusaurus/theme-mermaid',
+    'docusaurus-theme-search-typesense',
+  ],
+
   themeConfig: {
     colorMode: {
       disableSwitch: false
@@ -23,11 +46,6 @@ module.exports = {
         {to: 'blog', label: 'blog', position: 'right'},
         {to: 'https://github.com/hamlet-io/discussions/discussions', label: 'discussions', position: 'right'},
       ],
-    },
-    algolia: {
-      appId: 'hamlet',
-      apiKey: '919c25df3c384329b25098f1dcb5c5a6',
-      indexName: 'hamlet'
     },
     footer: {
       style: 'dark',
@@ -89,6 +107,20 @@ module.exports = {
     prism: {
       theme: require("./src/theme/hamlet"),
       additionalLanguages: ['groovy']
+    },
+    typesense: {
+      typesenseCollectionName: typesense_index_name,
+
+      typesenseServerConfig: {
+        nodes: [
+          {
+            host: typesense_host,
+            port: typesense_port,
+            protocol: typesense_protocol,
+          }
+        ],
+        apiKey: typesense_search_api_key,
+      }
     }
   },
   presets: [
@@ -101,7 +133,7 @@ module.exports = {
           routeBasePath: '/',
           // Please change this to your repo.
           editUrl: 'https://github.com/hamlet-io/docs/edit/master/',
-          remarkPlugins: [require("mdx-mermaid")],
+          remarkPlugins: [],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -109,9 +141,12 @@ module.exports = {
         blog : {
           blogSidebarTitle: "All our posts",
           blogSidebarCount: "ALL",
-          remarkPlugins: [require("mdx-mermaid")],
+          remarkPlugins: [],
         }
       },
     ],
   ],
+
 };
+
+module.exports = config;
